141dc18a26a9c8d35d3083cfe7c57f36448dff71
JoaoArouca <jarouca21@gmail.com>
2024-01-25 22:06:17 -0300

    feat: create user request with zod validation

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,9 @@
-export class CreateUser {
-  name: string
-  password: string
-  email: string
-  role: number
+import { z } from 'zod'
 
-  constructor() {
-    this.name = ''
-    this.email = ''
-    this.password = ''
-    this.role = 1
-  }
-}
+// export const RolesSchema = z.enum(roles)
+export const CreateUserRequestSchema = z.object({
+  name: z.string().min(3),
+  password: z.string().min(6),
+  email: z.string().email(),
+  role: z.enum(['CUSTOMER', 'ADMIN', 'INTERNAL']).default('CUSTOMER'),
+})
